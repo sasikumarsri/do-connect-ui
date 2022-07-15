33d2064af8d15033543ac57b676e3205951f94e0
sasikumarsri <sasikumar.dharmaraj@gmail.com>
2022-07-15 19:59:29 +0530

    home component changes

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -6,7 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  questionsAndAnswers: any = [
+    {
+      question : {
+        qus : 'ajbdsdfasdf sfuj sdfjsdbf sfb sfsduf sjfasudf sdfsfbusjfasdfusbf sf sjfbsdjf sdaf sjdf sdfuasdbfsd fsjdbfjskd fsjdbfjsd fasd fjasd fjsdfjsd fjksbadfms dfkjsdbfjs dfesd fjsadbnfmasd fjksabfjs fjsd fsjadbfjsadn fas dfjsdfjsd fasd fjsdnfjms fsd fjasdbfjsd fj fsdfjsdnfjsdbfjasdfsdfjsd fsand f',
+        userName: 'Test question',
+        userType: 'USER',
+        userProfile: '',
+        id: 21
+      }, 
+      answers: [
+        {
+          ans: 'dsadsadas das das dd asd ads sada sdas dada sdas dasd as dasd dad asd asdas das ',
+          userName: 'Test answer',
+          userType: 'USER',
+          userProfile: '',
+          id: 31
+        },
+        {
+          ans: 'dsadsadas das das dd asd ads sada sdas dada sdas dasd as dasd dad asd asdas das ',
+          userName: 'Test answer2',
+          userType: 'USER',
+          userProfile: '',
+          id: 32
+        },
+        {
+          ans: 'dsadsadas das das dd asd ads sada sdas dada sdas dasd as dasd dad asd asdas das ',
+          userName: 'Test answer3',
+          userType: 'USER',
+          userProfile: '',
+          id: 33
+        },
+        {
+          ans: 'dsadsadas das das dd asd ads sada sdas dada sdas dasd as dasd dad asd asdas das ',
+          userName: 'Test answer4',
+          userType: 'USER',
+          userProfile: '',
+          id: 34
+        }
+      ]
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MatCardModule } from "@angular/material/card"
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -12,6 +16,10 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule
   ]
 })
 export class HomeModule { }
